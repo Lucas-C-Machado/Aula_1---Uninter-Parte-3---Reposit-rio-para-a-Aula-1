d748cf46dfb31c71ba4606a4625b9e33352dfad1
Lucas C. Machado <65616970+Lucas-C-Machado@users.noreply.github.com>
2025-02-09 20:22:49 -0300

    Passagem de arquivos
    Passei alguns dos arquivos os quais estão na pasta propriamente dita do Professor, infelizmente não foram todos pois a biblioteca numpy é "muito grande".

---
+++ Tutoriais.py
@@ -0,0 +1,8 @@
+print ("Hello Word");
+print ("Meu nome é Lucas");
+
+import pygame as pyg; #Biblioteca "pygame" agora recebeu o nome de pyg (Abreviação)
+import numpy as np; #Biblioteca "numpy" agora recebeu o nome de np (Abreviação)
+
+
+
